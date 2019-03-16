838d94a8901618446f2221dfa2a89ad87a3be585
charafau <gorudonu@gmail.com>
2019-03-16 23:59:09 +0000

    added exports for types

--- lib/jikan_dart.dart
+++ lib/jikan_dart.dart
@@ -4,5 +4,42 @@
 library jikan_dart;
 
 export 'src/jikan_api.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/request_type/anime_request_type.dart';
+export 'src/model/aired_dto.dart';
+export 'src/model/anime_episode_dto.dart';
+export 'src/model/anime_episodes_dto.dart';
+export 'src/model/article_dto.dart';
+export 'src/model/forum_dto.dart';
+export 'src/model/last_post_dto.dart';
+export 'src/model/more_info_dto.dart';
+export 'src/model/picture_dto.dart';
+export 'src/model/promo_dto.dart';
+export 'src/model/recommendation_dto.dart';
+export 'src/model/score_dto.dart';
+export 'src/model/scores_dto.dart';
+export 'src/model/stats_dto.dart';
+export 'src/model/top_dto.dart';
+export 'src/model/top_type.dart';
+export 'src/model/genre/genre.dart';
+export 'src/model/genre/genre_list_dto.dart';
+export 'src/model/genre/genre_type.dart';
+export 'src/model/genre/mal_url_dto.dart';
+export 'src/model/manga/manga_character_dto.dart';
+export 'src/model/producer/producer_meta_dto.dart';
+export 'src/model/producer/producers_dto.dart';
+export 'src/model/schedule/schedule_dto.dart';
+export 'src/model/schedule/week_day.dart';
+export 'src/model/season/anime_dto.dart';
+export 'src/model/season/genre_dto.dart';
+export 'src/model/season/producer_dto.dart';
+export 'src/model/season/season.dart';
+export 'src/model/season/season_dto.dart';
+export 'src/model/user/anime_stats_dto.dart';
+export 'src/model/user/favorite_item_dto.dart';
+export 'src/model/user/favorites_dto.dart';
+export 'src/model/user/history_result_dto.dart';
+export 'src/model/user/history_result_item_dto.dart';
+export 'src/model/user/manga_stats_dto.dart';
+export 'src/model/user/meta_dto.dart';
+export 'src/model/user/profile_result_dto.dart';
+export 'src/model/user/user_request_type.dart';
