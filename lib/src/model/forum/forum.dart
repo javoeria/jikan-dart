1ee896dd0e2df3b73c15f363b0696f7a36c84a55
Javier <javier.rb_96@hotmail.com>
2021-02-06 17:51:03 +0100

    move forum models to new folder

--- lib/src/model/forum/forum.dart
+++ lib/src/model/forum/forum.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 
 import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
-import 'package:jikan_api/src/model/common/last_post.dart';
+import 'package:jikan_api/src/model/forum/last_post.dart';
 import 'package:jikan_api/src/model/serializers.dart';
 
 part 'forum.g.dart';
